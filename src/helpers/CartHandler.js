98750bbb6aee1a4296bfd4a5d0dd5062b7f8a010
Tawhidul Islam Khan <tawhid.developer@gmail.com>
2019-08-12 00:46:13 +0600

    non array foreach issue fixed

--- src/helpers/CartHandler.js
+++ src/helpers/CartHandler.js
@@ -61,108 +61,127 @@ class CartHandler {
         return total;
     }
 
-    totalBeforeTax(){
+    totalBeforeTax() {
 
         let beforeTax = 0;
         let cartItems = this.getCartItems();
         let that = this;
-        cartItems.forEach((el) => {
-            let qty = that.getQty(el.key);
-            beforeTax += Math.round((el.price + el.shipping) * qty);
-        })
+        if (cartItems) {
+            cartItems.forEach((el) => {
+                let qty = that.getQty(el.key);
+                beforeTax += Math.round((el.price + el.shipping) * qty);
+            })
+        }
         return beforeTax;
     }
 
-    getTax(){
+    getTax() {
 
         let tax = 0;
         let cartItems = this.getCartItems();
         let that = this;
-        cartItems.forEach((el) => {
-            let qty = that.getQty(el.key);
-            tax += Math.round(((el.price + el.shipping) * qty) * 0.1);
-        })
+        if (cartItems) {
+            cartItems.forEach((el) => {
+                let qty = that.getQty(el.key);
+                tax += Math.round(((el.price + el.shipping) * qty) * 0.1);
+            })
+        }
         return tax;
     }
 
-    getQty(key){
+    getQty(key) {
         let cart = this.getCart();
-        let qty=0;
-        cart.forEach((el) => {
-            if(el.key == key){
-                qty = el.qty;
-            }
-        });
+        let qty = 0;
+        if (cart) {
+            cart.forEach((el) => {
+                if (el.key == key) {
+                    qty = el.qty;
+                }
+            });
+        }
 
         return qty;
     }
 
-    getTotal(){
+    getTotal() {
 
         let total = 0;
         let cartItems = this.getCartItems();
         let that = this;
-        cartItems.forEach((el) => {
-            let qty = that.getQty(el.key);
-            total += Math.round((el.price + el.shipping) * qty);
-        })
+        if (cartItems) {
+            cartItems.forEach((el) => {
+                let qty = that.getQty(el.key);
+                total += Math.round((el.price + el.shipping) * qty);
+            })
+        }
         return total + this.getTax();
     }
 
-    getShippingPrice(){
+    getShippingPrice() {
 
         let shippingPrice = 0;
         let cartItems = this.getCartItems();
         let that = this;
-        cartItems.forEach((el) => {
-            let qty = that.getQty(el.key);
-            shippingPrice += Math.round(el.shipping*qty);
-        })
+        if (cartItems) {
+
+            cartItems.forEach((el) => {
+                let qty = that.getQty(el.key);
+                shippingPrice += Math.round(el.shipping * qty);
+            })
+
+        }
         return shippingPrice;
     }
 
-    getSubtotal(){
+    getSubtotal() {
 
         let subTotal = 0;
         let cartItems = this.getCartItems();
         let that = this;
-        cartItems.forEach((el) => {
-            let qty = that.getQty(el.key);
-            subTotal += Math.round(el.price*qty);
-        })
+        if(cartItems){
+            cartItems.forEach((el) => {
+                let qty = that.getQty(el.key);
+                subTotal += Math.round(el.price * qty);
+            })
+        }
         return subTotal;
     }
-    getCart(){
+
+    getCart() {
         let cart = JSON.parse(localStorage.getItem('emaJohn/carts/user-234'))
         return cart;
     }
-    setCart(cart){
+
+    setCart(cart) {
         localStorage.setItem('emaJohn/carts/user-234', JSON.stringify(cart))
     }
-    getCartItems(){
+
+    getCartItems() {
 
         let cartItems = [];
         let cart = this.getCart();
         let cartKeys = [];
         let cartQty = [];
         let that = this;
-        cart = cart.forEach((el) => {
-            cartKeys.push(el.key);
-            cartQty.push(el.qty);
-        });
+        if(cart){
+            cart = cart.forEach((el) => {
+                cartKeys.push(el.key);
+                cartQty.push(el.qty);
+            });
+        }
 
         fakeData.forEach((el) => {
-            if(cartKeys.includes(el.key)){
+            if (cartKeys.includes(el.key)) {
                 cartItems.push(el)
             }
         })
         return cartItems;
     }
 
-    deleteItem(key){
+    deleteItem(key) {
         let cart = this.getCart();
-        cart = cart.filter((el) =>{
-            if(el.key !== key){
+        cart = cart.filter((el) => {
+            if (el.key !== key) {
                 return el;
             }
         })
